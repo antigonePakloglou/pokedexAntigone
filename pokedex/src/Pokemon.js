9801d9439322f4a910dc0efaf917a954d9b5ff24
Antigone Pakloglou <antigone1612@hotmail.fr>
2022-05-17 13:48:32 +0200

    ajout dossier img

--- pokedex/src/Pokemon.js
+++ pokedex/src/Pokemon.js
@@ -1,9 +1,27 @@
 import {Card, Button} from 'react-bootstrap';
-const Pokemon = ({name, type, vitesse}) => {
-  
+const Pokemon = ({id,name, type, vitesse}) => {
+    console.log(id.toString().length);
+
+    let img;
+    switch (id.toString().length) {
+        case 1:
+            img = 'src/img/00'+id.toString()+'.png';
+            console.log(img);
+            break;
+        case 2:
+            img = 'src/img/0'+id.toString()+'.png';
+            console.log(img);
+        case 3:
+            img = 'src/img/'+id.toString()+'.png';
+            console.log(img);
+            break;
+        default:
+            img = 'src/img/001.png';
+    }
+
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src='img\001.png' />
             <Card.Body>
                 <Card.Title>{name['french']}</Card.Title>
                 <Button variant="primary">Voir plus</Button>
